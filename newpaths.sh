dd3cb1379e7f5f1d2f73643c501a177a9b712c24
Holger Wolff <holger.wolff@monash.edu>
2019-10-28 16:13:12 +1100

    add script to be sourced during build of model

---
+++ newpaths.sh
@@ -0,0 +1,6 @@
+export FPATH=/short/w35/hxw599/oasis3-mct/Linux/build/lib/psmile.MPI1/:/short/w35/hxw599/oasis3-mct/Linux/build/lib/mctdir/mct:/short/w35/hxw599/oasis3-mct/Linux/build/lib/scrip:$FPATH
+export LIBRARY_PATH=/short/w35/hxw599/oasis3-mct/Linux/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=/short/w35/hxw599/oasis3-mct/Linux/lib:$LD_LIBRARY_PATH
+export RPATH=/short/w35/hxw599/oasis3-mct/Linux/lib:$RPATH
+export LD_RUN_PATH=/short/w35/hxw599/oasis3-mct/Linux/lib:$LD_RUN_PATH
+
