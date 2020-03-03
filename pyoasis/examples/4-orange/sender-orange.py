4263b6b16d8b2509b999e7661e47bfa4ddc6d494
Philippe Gambron <philippe.gambron@stfc.ac.uk>
2020-03-03 11:37:44 +0000

    Example 4
    Former-commit-id: 41814b6309172be6560fb2e80edeb2e58842512d

---
+++ pyoasis/examples/4-orange/sender-orange.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import pyoasis
+import numpy
+
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+
+component_name = "sender-orange"
+print("Component name: " + component_name)
+
+comp = pyoasis.Component(component_name, True, comm)
+
+print("Component id: " + str(comp.get_id()))
+
+local_comm = comp.get_localcomm()
+print("Local communicator: " + str(local_comm))
+
+coupl_comm = comp.create_couplcomm(1, local_comm)
+print("Coupling communicator: " + str(coupl_comm))
+
+comm_rank = comp.get_localcomm_rank()
+comm_size = comp.get_localcomm_size()
+
+n_points = 16
+extent = int(n_points/comm_size)
+offset = comm_rank*extent
+
+offsets = [offset]
+extents = [extent]
+
+partition = pyoasis.OrangePartition(offsets, extents)
+print("Partition id: " + str(partition.id))
+
+variable = pyoasis.Var(partition.id, "FSENDOCN", 1, 1,
+                       pyoasis.params.OASIS_OUT.value,
+                       pyoasis.params.OASIS_REAL.value)
+print("Variable id: " + str(variable.id))
+
+comp.enddef()
+
+date = int(0)
+
+field = pyoasis.Array(numpy.zeros(extent))
+for i in range(extent):
+    field[i] = offset + i
+
+variable.put(date, field)
+
+pyoasis.terminate()
