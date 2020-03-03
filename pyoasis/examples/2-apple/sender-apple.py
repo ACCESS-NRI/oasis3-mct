15911443cdb3a78394f55c4485cf41c37203b0a9
Philippe Gambron <philippe.gambron@stfc.ac.uk>
2020-03-03 11:10:47 +0000

    Example 2
    Former-commit-id: bee1639623700601a583c5b176ba918ba2b965a4

--- pyoasis/examples/2-apple/sender-apple.py
+++ pyoasis/examples/2-apple/sender-apple.py
@@ -18,33 +18,33 @@ print("Local communicator: " + str(local_comm))
 coupl_comm = comp.create_couplcomm(1, local_comm)
 print("Coupling communicator: " + str(coupl_comm))
 
-comm_rank=comp.get_localcomm_rank()
-comm_size=comp.get_localcomm_size()
+comm_rank = comp.get_localcomm_rank()
+comm_size = comp.get_localcomm_size()
 
-n_points=16
+n_points = 16
 
-local_size=int(n_points/comm_size)
-offset=comm_rank*local_size
-      
-partition=pyoasis.ApplePartition(offset, local_size)
+local_size = int(n_points/comm_size)
+offset = comm_rank*local_size
+
+partition = pyoasis.ApplePartition(offset, local_size)
 print("Partition id: " + str(partition.id))
 
-variable=pyoasis.Var(partition.id, "FSENDOCN", 1, 1,
-                     pyoasis.params.OASIS_OUT.value,
-                     pyoasis.params.OASIS_REAL.value)
+variable = pyoasis.Var(partition.id, "FSENDOCN", 1, 1,
+                       pyoasis.params.OASIS_OUT.value,
+                       pyoasis.params.OASIS_REAL.value)
 print("Variable id: " + str(variable.id))
 
 comp.enddef()
 
-date=int(0);
+date = int(0)
 
-field=pyoasis.Array(numpy.zeros(local_size))
+field = pyoasis.Array(numpy.zeros(local_size))
 
 for i in range(local_size):
     field[i] = offset + i
-    
+
 print("Sent data: "+str(field))
 
-variable.put(date, field); 
+variable.put(date, field)
 
 pyoasis.terminate()
