0e8d0e3982b31f209a449590d043fddba215fb88
Philippe Gambron <philippe.gambron@stfc.ac.uk>
2020-03-06 16:41:50 +0000

    Example 1 Fortran
    Former-commit-id: 0353509c32cef88e58e30ecab37fdf9858722ac8

--- pyoasis/init.sh
+++ pyoasis/init.sh
@@ -1,7 +1,7 @@
 OPT=${HOME}/opt
 OASIS=${HOME}/oasis3-mct/
 PYOASIS=${OASIS}/pyoasis
-MODULES=${PYOASIS}/oasis/modules
+
 
 export C_INCLUDE_PATH=${PYOASIS}:${OPT}/include:/usr/local/lib:${C_INCLUDE_PATH}
 export CPLUS_INCLUDE_PATH=${OPT}/include:${CPLUS_INCLUDE_PATH}
