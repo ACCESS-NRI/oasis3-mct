004f76904b8c4494ab209ecabbfa6b9608789a14
Philippe Gambron <philippe.gambron@stfc.ac.uk>
2020-03-09 09:47:48 +0000

    Example 2 Fortran
    Former-commit-id: 8113e09e2870abb27d522cf346539334cc2f7112

---
+++ pyoasis/examples/2-apple/fortran/receiver.F90
@@ -0,0 +1,91 @@
+program receiver
+  use mod_oasis
+  implicit none
+  integer :: i, kinfo
+  integer :: comp_id, local_comm, coupl_comm
+  integer :: n_points, var_type, part_id
+  integer :: part_params(3)
+  integer :: var_id, var_nodims(2), var_actual_shape(1), date
+  character(len=8) :: comp_name = "receiver"
+  character(len=8) :: var_name = "FRECVATM"
+  real :: field(16), error, epsilon
+
+  print *, "Component name: ", comp_name
+	
+  call oasis_init_comp(comp_id, comp_name, kinfo)
+  if(kinfo<0) then
+    print *, "Error in oasis_init_comp: ", kinfo
+    return
+  endif
+  print *, "Component ID: ", comp_id
+  
+  call oasis_get_localcomm(local_comm, kinfo)
+  if(kinfo<0) then
+    print *, "Error in oasis_get_localcomm: ", kinfo
+    return
+  endif
+  print *, "local_comm=",local_comm
+
+  call oasis_create_couplcomm(1, local_comm, coupl_comm, kinfo)
+  print *, "coupl_comm ", coupl_comm
+  if(kinfo<0) then
+    print *, "Error in oasis_create_couplcomm: ", kinfo
+    return
+  endif
+  print *, "coupl_comm ", coupl_comm
+
+  n_points=16
+  part_params=(/0, 0, n_points/)
+  call oasis_def_partition(part_id, part_params, kinfo)
+  if(kinfo<0) then
+    print *, "Error in oasis_def_partition: ", kinfo
+    return
+  endif
+  print *, "part_id: ", part_id
+	
+  var_nodims=(/1, 1/)
+  var_actual_shape=1
+  print *, "var_name: ", var_name
+  call oasis_def_var(var_id, var_name, part_id, var_nodims, OASIS_IN, &
+                    var_actual_shape, OASIS_REAL, kinfo)
+  if(kinfo<0 .or. var_id<0) then
+    print *, "Error in oasis_def_partition: ", kinfo
+    return
+  endif 
+  print *, "var_id: ", var_id
+  
+  call oasis_enddef(kinfo)
+  if(kinfo<0) then
+    print *, "Error in oasis_enddef: ", kinfo
+    return
+  endif
+	
+  date=0
+	print *, "BEFORE GET"
+ call oasis_get(var_id, date, field, kinfo)
+ print *, "AFTER GET"
+  if(kinfo<0) then
+    print *, "Error in oasis_get: ", kinfo
+    return
+  endif
+
+  
+  do i=0, n_points-1
+    field(i)=i
+  end do
+  
+  call oasis_terminate(kinfo)
+  if(kinfo<0) then
+    print *, "Error in oasis_terminate: ", kinfo
+  endif
+
+  epsilon=1e-8
+  error=0
+  do i = 0, n_points-1
+    error=error+abs(field(i)-i)
+  end do
+  if(error<epsilon) then
+    print *, "Data received successfully" 
+  endif
+
+end program receiver
---
+++ pyoasis/examples/2-apple/fortran/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir -p work
+
+srcdir=`pwd`
+datadir=$srcdir/data
+casename=`basename $srcdir`
+
+exe1=sender-apple
+exe2=receiver
+
+n1=4
+n2=1
+
+rundir=$srcdir/work
+
+rm -fr $rundir
+mkdir -p $rundir
+
+cp -f $srcdir/$exe1 $rundir/.
+cp -f $srcdir/$exe2 $rundir/.
+
+cp -f $datadir/namcouple $rundir/.
+
+cd $rundir
+
+mpirun --oversubscribe -np $n1 $exe1 : -np $n2  $exe2
