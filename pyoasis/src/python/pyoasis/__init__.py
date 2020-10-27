37792ebdcb178b25b4c8b84e3ae2256e38258568
Andrea Piacentini <piacentini.palm@gmail.com>
2020-10-27 07:18:03 +0100

    [ap-dev-pyoasis] Avoid most of the mpi hanging if python exceptions are intercepted and converted to mpi4py abort. Philippe, Rupert, do  you like it in the __init__.py or would you prefer to make this hook more explicit?
    Former-commit-id: 506195e7041a0d4c58af74401c518557799a23c7

--- pyoasis/src/python/pyoasis/__init__.py
+++ pyoasis/src/python/pyoasis/__init__.py
@@ -8,3 +8,32 @@ from pyoasis.component import *
 from pyoasis.terminate import *
 from pyoasis.partition import *
 from pyoasis.var import *
+
+# Global error handler
+def global_except_hook(exctype, value, traceback):
+    """
+    Shut down a whole MPI application if one of the processes fails
+    """
+    import sys
+    try:
+        import mpi4py.MPI
+        sys.stderr.write("\n*****************************************************\n" +
+                         "Uncaught exception was detected on rank {}. \n".format(
+                             mpi4py.MPI.COMM_WORLD.Get_rank()))
+        from traceback import print_exception
+        print_exception(exctype, value, traceback)
+        sys.stderr.flush()
+    finally:
+        try:
+            import mpi4py.MPI
+            mpi4py.MPI.COMM_WORLD.Abort(1)
+        except Exception as e:
+            sys.stderr.write("*****************************************************\n")
+            sys.stderr.write("Sorry, we failed to stop MPI, this process will hang.\n")
+            sys.stderr.write("*****************************************************\n")
+            sys.stderr.flush()
+            raise e
+
+
+import sys
+sys.excepthook = global_except_hook
