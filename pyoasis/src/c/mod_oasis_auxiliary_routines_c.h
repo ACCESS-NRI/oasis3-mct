53425388324621adad3e41b2f216e1ef2f1d9c60
Philippe <philippe@herodium.esc.rl.ac.uk>
2020-10-29 13:48:33 +0000

    Add new functions to C wrapper
    Former-commit-id: 4163a6546a045ff57939f3331abd432914cb5435

--- pyoasis/src/c/mod_oasis_auxiliary_routines_c.h
+++ pyoasis/src/c/mod_oasis_auxiliary_routines_c.h
@@ -34,6 +34,16 @@ extern void get_intercomm_iso(int* new_comm, char** cdnam, int* error);
 
 extern void get_intracomm_iso(int* new_comm, char** cdnam, int* error);
 
+extern void oasis_set_debug_iso(int* debug, int* kinfo);
+
+extern void oasis_get_debug_iso(int* debug, int* kinfo);
+ 
+extern void oasis_put_inquire_iso(int* varid, int* msec, int* kinfo);
+
+extern void oasis_get_ncpl_iso(int* varid, int* ncpl, int* kinfo);
+
+extern void oasis_get_freqs_iso(int* varid, int* mop, int* ncpl, int* cpl_freqs, int* kinfo);
+
 
 void get_localcomm(int* localcomm, int* kinfo);
 
@@ -42,6 +52,16 @@ void create_couplcomm(int icpl, int allcomm, int* cplcomm, int* error);
 void set_couplcomm(int localcomm, int* kinfo);
 
 void get_intracomm(int new_comm, char* cdnam, int* error);
+
+void oasis_set_debug(int debug, int* kinfo);
+
+void oasis_get_debug(int* debug, int* kinfo);
+ 
+void oasis_put_inquire(int varid, int msec, int* kinfo);
+
+void oasis_get_ncpl(int varid, int* ncpl, int* kinfo);
+
+void oasis_get_freqs(int varid, int mop, int ncpl, int* cpl_freqs, int* kinfo);
 
 
 #endif
