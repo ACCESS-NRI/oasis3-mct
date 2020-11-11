2a7a13957f3b0a8391bfdcb92a05efd353b85379
Philippe Gambron <philippe.gambron@stfc.ac.uk>
2020-11-11 12:14:47 +0000

    Cleaned up names
    Former-commit-id: 9edb2e3347247d37551f66df4a876cc5b4ef8e1c

--- pyoasis/src/python/pyoasis/var.py
+++ pyoasis/src/python/pyoasis/var.py
@@ -30,36 +30,36 @@ class Var:
     """
     Variable data
 
-    :param string cdport: name
+    :param string var_name: name
     :type partition: partition identifier
-    :param kinout: flag indicating whether the data is outgoing \
+    :param inout: flag indicating whether the data is outgoing \
                    or ingoing
-    :type kinout: pyoasis.OasisParameter
+    :type inout: pyoasis.OasisParameter
     :param int bundle_size: size of a bundle of fields
     :raises OasisException: if OASIS is unable to initialise \
                             the variable data 
     :raises PyOasisException: if an incorrect parameter is supplied
     """
 
-    def __init__(self, cdport, partition, kinout, bundle_size=1):
+    def __init__(self, var_name, partition, inout, bundle_size=1):
         """Constructor"""
 
         pyoasis.check_types([str, pyoasis.Partition, pyoasis.OasisParameters, int],
-                            [cdport, partition, kinout, bundle_size])
-        if len(cdport) == 0:
+                            [var_name, partition, kinout, bundle_size])
+        if len(var_name) == 0:
             raise pyoasis.PyOasisException("Name empty")
         _id_partition = partition._id
         self._partition_local_size = partition.local_size
         if _id_partition < 0:
             raise pyoasis.PyOasisException("Partition identifier <0.")
-        if not (kinout == pyoasis.OasisParameters.OASIS_IN
-                or kinout == pyoasis.OasisParameters.OASIS_OUT):
-            raise pyoasis.PyOasisException("kinout parameter neither OASIS_IN or OASIS_OUT.")
+        if not (inout == pyoasis.OasisParameters.OASIS_IN
+                or inout == pyoasis.OasisParameters.OASIS_OUT):
+            raise pyoasis.PyOasisException("inout parameter neither OASIS_IN or OASIS_OUT.")
         if bundle_size < 1:
             raise pyoasis.PyOasisException("Bundle size <1.")
-        self.name = cdport
+        self.name = var_name
         self.bundle_size = bundle_size
-        self.direction = kinout
+        self.direction = inout
         id_var_nodims = [1, bundle_size]
         return_value = pyoasis.mod_oasis_var.def_var(_id_partition, self.name, id_var_nodims,
                                                      self.direction.value)
@@ -91,11 +91,11 @@ class Var:
         """
         return self.name
 
-    def put(self, kstep, field, write_restart=False):
+    def put(self, time, field, write_restart=False):
         """
         Sends data to another model.
 
-        :param int kstep: model time (in seconds)
+        :param int time: model time (in seconds)
         :param pyoasis.asarray field: data
         :param bool write_restart: optional flag for writing a restart file
 
@@ -103,10 +103,10 @@ class Var:
          data to the other component
         :raises PyOasisException: if an incorrect parameter is supplied 
         """
-        pyoasis.check_types([int, numpy.ndarray, bool], [kstep, field, write_restart])
+        pyoasis.check_types([int, numpy.ndarray, bool], [time, field, write_restart])
         if(not self._check_size(field)):
             raise pyoasis.OasisException("Field of the wrong size", error)
-        error = pyoasis.mod_oasis_getput_interface.put(self._id, kstep, field, write_restart)
+        error = pyoasis.mod_oasis_getput_interface.put(self._id, time, field, write_restart)
         if error < 0:
             raise pyoasis.OasisException("Error in sending data to another component", error)
 
@@ -115,21 +115,21 @@ class Var:
         except ValueError:
             raise pyoasis.OasisException("Error in put: returned value not mapped to Oasis parameters", -1)
 
-    def get(self, kstep, field):
+    def get(self, time, field):
         """
         Gets data from another model.
 
-        :param int kstep: model time (in seconds)
+        :param int time: model time (in seconds)
         :param pyoasis.asarray field: data
 
         :raises OasisException: if OASIS is unable to receive \
         data from the other component
         :raises PyOasisException: if an incorrect parameter is supplied
         """
-        pyoasis.check_types([int, numpy.ndarray], [kstep, field])
+        pyoasis.check_types([int, numpy.ndarray], [time, field])
         if(not self._check_size(field)):
             raise pyoasis.OasisException("Field of the wrong size", error)
-        error = pyoasis.mod_oasis_getput_interface.get(self._id, kstep, field)
+        error = pyoasis.mod_oasis_getput_interface.get(self._id, time, field)
         if error < 0:
             raise pyoasis.OasisException("Error in getting data from another component", error)
 
@@ -141,18 +141,18 @@ class Var:
     def __str__(self):
         return "Variable data: name: " + self.name + ", id: " + str(self._id)
 
-    def put_inquire(self, kstep):
+    def put_inquire(self, time):
         """
         :returns: return code expected at a specified time 
         for a given variable
         :rtype: integer
-        :param int kstep: model time (in seconds)
+        :param int time: model time (in seconds)
 
         :raises OasisException: if OASIS is unable to obtain the return code
         :raises PyOasisException: if an incorrect parameter is supplied
         """
-        pyoasis.check_types([int], [kstep])
-        rcode = pyoasis.mod_oasis_auxiliary_routines.put_inquire(self._id, kstep)
+        pyoasis.check_types([int], [time])
+        rcode = pyoasis.mod_oasis_auxiliary_routines.put_inquire(self._id, time)
         try:
             return pyoasis.OasisParameters(rcode)
         except ValueError:
