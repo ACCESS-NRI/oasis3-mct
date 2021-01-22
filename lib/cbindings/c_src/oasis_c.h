59c90b07103a943d9a8aaf17bf7f9811e2cc109c
Andrea Piacentini <piacentini.palm@gmail.com>
2021-01-23 00:56:30 +0100

    [dev-pyoasis] Set back comment syntax to -ansi -pedantic compliancy
    Former-commit-id: 1bd0d7860692baf2b5e725fb93d445efeb67b153

--- lib/cbindings/c_src/oasis_c.h
+++ lib/cbindings/c_src/oasis_c.h
@@ -1,20 +1,20 @@
-// pyOASIS - A Python wrapper for OASIS
-// Authors: Philippe Gambron, Rupert Ford
-// Copyright (C) 2019 UKRI - STFC
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-
-// A copy of the GNU Lesser General Public License, version 3, is supplied
-// with this program, in the file lgpl-3.0.txt. It is also available at
-// <https://www.gnu.org/licenses/lgpl-3.0.html>.
+/* pyOASIS - A Python wrapper for OASIS
+   Authors: Philippe Gambron, Rupert Ford
+   Copyright (C) 2019 UKRI - STFC
+  
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or any later version.
+  
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Lesser General Public License for more details.
+  
+   A copy of the GNU Lesser General Public License, version 3, is supplied
+   with this program, in the file lgpl-3.0.txt. It is also available at
+   <https://www.gnu.org/licenses/lgpl-3.0.html>. */
 
 
 #ifndef __OASIS_C_ROUTINES_C_H__
@@ -119,7 +119,7 @@ enum return_codes
   OASIS_Waitgroup     = 14
 };
 
-// C interfaces with MPI Communicator defined as C MPI_Comm (for C)
+/* C interfaces with MPI Communicator defined as C MPI_Comm (for C) */
 
 int oasis_c_get_localcomm(MPI_Comm* localcomm);
 
--- lib/cbindings/c_src/oasis_c_iso.h
+++ lib/cbindings/c_src/oasis_c_iso.h
@@ -1,20 +1,20 @@
-// pyOASIS - A Python wrapper for OASIS
-// Authors: Philippe Gambron, Rupert Ford
-// Copyright (C) 2019 UKRI - STFC
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-
-// A copy of the GNU Lesser General Public License, version 3, is supplied
-// with this program, in the file lgpl-3.0.txt. It is also available at
-// <https://www.gnu.org/licenses/lgpl-3.0.html>.
+/* pyOASIS - A Python wrapper for OASIS
+   Authors: Philippe Gambron, Rupert Ford
+   Copyright (C) 2019 UKRI - STFC
+  
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation, either version 3 of the
+   License, or any later version.
+  
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Lesser General Public License for more details.
+  
+   A copy of the GNU Lesser General Public License, version 3, is supplied
+   with this program, in the file lgpl-3.0.txt. It is also available at
+   <https://www.gnu.org/licenses/lgpl-3.0.html>. */
 
 
 #ifndef __OASIS_C_ISO_ROUTINES_C_H__
@@ -25,7 +25,7 @@
 #include <mpi.h>
 
 
-// FORTRAN interfaces
+/* FORTRAN interfaces */
 
 void oasis_get_localcomm_iso(int* localcomm, int* kinfo);
 
@@ -88,7 +88,7 @@ void oasis_abort_iso(const int* id_compid, const char** cd_routine, const char**
 void oasis_def_var_iso(int* id_nports, const char** cdport, const int* id_part, const int* bundle_size, const int* kinout, const int* ktype, int* kinfo);
 
 
-// C interfaces with MPI Communicator defined as int (for python or C++)
+/* C interfaces with MPI Communicator defined as int (for python or C++) */
 
 #if defined(c_plusplus) || defined(__cplusplus)
 extern "C" {
