77bb7c2f1380b64fb43af947b315d4c9dec9ad0b
Coquart <coquart@cerfacs.fr>
2021-04-22 16:19:58 +0200

    Update Signed-off-by: Coquart <coquart@cerfacs.fr>
    Former-commit-id: 62cb2c2a1ca0af12b144204f91dc3b25fdbea3d1

--- examples/spoc/spoc_regridding/run_regrid.sh
+++ examples/spoc/spoc_regridding/run_regrid.sh
@@ -39,7 +39,7 @@ else
 fi
 ##
 ## User's choice of computing architecture
-arch=pgi_openmpi_openmp_linux  # nemo_lenovo_intel_impi_openmp, kraken_intel_impi_openmp,
+arch=pgi20.4_openmpi_openmp_linux  # nemo_lenovo_intel_impi_openmp, kraken_intel_impi_openmp,
               # training_computer, gfortran_openmpi_openmp_linux, belenos, mac
 	      # pgi_openmpi_openmp_linux, 
 	      # pgi20.4_openmpi_openmp_linux (not work with 4.0)
@@ -287,11 +287,11 @@ elif [ ${arch} == gnu1020_openmpi_openmp_linux ]; then
     export OASIS_OMP_NUM_THREADS=$threads
     MPIRUN=/usr/local/openmpi/4.1.0_gcc1020/bin/mpirun
     echo 'Executing the model using '$MPIRUN
-    $MPIRUN -np $nproc_exe1 ./$exe1 : -np $nproc_exe2 ./$exe2 > runjob.err
+    $MPIRUN -oversubscribe -np $nproc_exe1 ./$exe1 : -np $nproc_exe2 ./$exe2 > runjob.err
 elif [ $arch == pgi20.4_openmpi_openmp_linux ]; then
     MPIRUN=/usr/local/pgi/linux86-64/20.4/mpi/openmpi-3.1.3/bin/mpirun
     echo 'Executing the model using '$MPIRUN
-    $MPIRUN -np $nproc_exe1 ./$exe1 : -np $nproc_exe2 ./$exe2 > runjob.err
+    $MPIRUN -oversubscribe -np $nproc_exe1 ./$exe1 : -np $nproc_exe2 ./$exe2 > runjob.err
 elif [ $arch == nemo_lenovo_intel_impi_openmp ]; then
     echo 'Submitting the job to queue using sbatch'
     sbatch $rundir/run_$casename.$arch
