6cc7d3421ad02718402bab7dbb1ea1f4fc1cbda4
valcke <valcke@cerfacs.fr>
2021-09-16 15:46:00 +0200

    Updated
    Former-commit-id: c057cfaa5cb150566424e469f49d0bc58159ddb2

--- examples/regrid_environment/function_ana.f90
+++ examples/regrid_environment/function_ana.f90
@@ -1,31 +1,37 @@
-SUBROUTINE function_ana(ni, nj, xcoor, ycoor, fnc_ana)
+MODULE function_ana
+!
+  IMPLICIT NONE
+!
+  CONTAINS
+!
+!****************************************************************************************
+SUBROUTINE function_ana1(ni, nj, xcoor, ycoor, fnc_ana)
+!****************************************************************************************
 
 !**** *function ana*  - calculate analytical function
-  !
-  IMPLICIT NONE
-  !
+
   INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND(12,307) ! double
-  !
+
   INTEGER, INTENT(IN) :: ni, nj
   REAL(kind=wp), DIMENSION(ni,nj), INTENT(IN)  :: xcoor, ycoor
   REAL(kind=wp), DIMENSION(ni,nj), INTENT(OUT) :: fnc_ana
-  !
+
   REAL (kind=wp), PARAMETER    :: dp_pi=3.14159265359
   REAL (kind=wp), PARAMETER    :: dp_conv = dp_pi/180.
   REAL(kind=wp)  :: dp_length, coef, coefmult
   INTEGER             :: i,j
   CHARACTER(LEN=7) :: cl_anaftype="fcos"
-  !
+
   DO j=1,nj
     DO i=1,ni
-!
+
       SELECT CASE (cl_anaftype)
       CASE ("fcos")
         dp_length = 1.2*dp_pi
         coef = 2.
         coefmult = 1.
         fnc_ana(i,j) = coefmult*(coef - COS( dp_pi*(ACOS( COS(xcoor(i,j)*dp_conv)*COS(ycoor(i,j)*dp_conv) )/dp_length)) )
-!
+
       CASE ("fcossin")
         dp_length = 1.d0*dp_pi
         coef = 21.d0
@@ -33,8 +39,138 @@ SUBROUTINE function_ana(ni, nj, xcoor, ycoor, fnc_ana)
         fnc_ana(i,j) = coefmult*(coef - COS( dp_pi*(ACOS( COS(ycoor(i,j)*dp_conv)*COS(ycoor(i,j)*dp_conv) )/dp_length)) * &
                                         SIN( dp_pi*(ASIN( SIN(xcoor(i,j)*dp_conv)*SIN(ycoor(i,j)*dp_conv) )/dp_length)) )
       END SELECT
-!
+
     ENDDO
   ENDDO
+
+END SUBROUTINE function_ana1
+
+!!****************************************************************************************
+!SUBROUTINE function_ana2(lon, lat, mask, fnc_ana)
+SUBROUTINE function_ana2(ni, nj, lon, lat, fnc_ana)
+!!****************************************************************************************
+
+!**** *function ana*  - calculate analytical function
+
+  INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND(12,307) ! double
+
+  INTEGER, INTENT(IN) :: ni, nj
+  REAL (kind=wp), DIMENSION(ni,nj), INTENT(IN)  :: lon, lat
+  REAL(kind=wp), DIMENSION(ni,nj), INTENT(OUT) :: fnc_ana
+
+  REAL (kind=wp), PARAMETER :: coef=2., dp_pi=3.14159265359
+  REAL (kind=wp) :: dp_length, dp_conv
+  INTEGER :: i, j
+
+  ! Analytical Gulf Stream
+  REAL (kind=wp) :: gf_coef, gf_ori_lon, gf_ori_lat, &
+                  & gf_end_lon, gf_end_lat, gf_dmp_lon, gf_dmp_lat
+  REAL (kind=wp) :: gf_per_lon 
+  REAL (kind=wp) :: dx, dy, dr, dth, dc, dr0, dr1
+
+  ! Parameters for analytical function
+  dp_length = 1.2*dp_pi
+  dp_conv = dp_pi/180.
+  gf_coef = 1.0 ! Coefficient for Gulf Stream term (0.0 = no Gulf Stream)
+  gf_ori_lon = -80.0 ! Origin of the Gulf Stream (longitude in deg)
+  gf_ori_lat =  25.0 ! Origin of the Gulf Stream (latitude in deg) 
+  gf_end_lon =  -1.8 ! End point of the Gulf Stream (longitude in deg)
+  gf_end_lat =  50.0 ! End point of the Gulf Stream (latitude in deg) 
+  gf_dmp_lon = -25.5 ! Point of the Gulf Stream decrease (longitude in deg)
+  gf_dmp_lat =  55.5 ! Point of the Gulf Stream decrease (latitude in deg) 
+
+!  ALLOCATE(fnc_ana(0:xy-1))
+
+  dr0 = SQRT(((gf_end_lon - gf_ori_lon)*dp_conv)**2 + &
+     & ((gf_end_lat - gf_ori_lat)*dp_conv)**2)
+  dr1 = SQRT(((gf_dmp_lon - gf_ori_lon)*dp_conv)**2 + &
+     & ((gf_dmp_lat - gf_ori_lat)*dp_conv)**2)
+
+  DO j=1,nj
+    DO i=1,ni
+
+      ! Original OASIS fcos analytical test function
+      fnc_ana(i,j)=(coef-COS(dp_pi*(ACOS(COS(lat(i,j)*dp_conv)*&
+                       & COS(lon(i,j)*dp_conv))/dp_length)))
+      gf_per_lon = lon(i,j)
+      IF (gf_per_lon > 180.0) gf_per_lon = gf_per_lon - 360.0
+      IF (gf_per_lon < -180.0) gf_per_lon = gf_per_lon + 360.0 
+      dx = (gf_per_lon - gf_ori_lon)*dp_conv
+      dy = (lat(i,j) - gf_ori_lat)*dp_conv
+      dr = SQRT(dx*dx + dy*dy)
+      dth = ATAN2(dy, dx)
+      dc = 1.3*gf_coef
+      IF (dr > dr0) dc = 0.0
+      IF (dr > dr1) dc = dc * COS(dp_pi*0.5*(dr-dr1)/(dr0-dr1))
+      fnc_ana(i,j) = fnc_ana(i,j) + &
+                       & (MAX(1000.0*SIN(0.4*(0.5*dr+dth) + &
+                       &  0.007*COS(50.0*dth) + 0.37*dp_pi),999.0) - 999.0) * dc
+
+    ENDDO
+  ENDDO
+
+END SUBROUTINE function_ana2
+
+!****************************************************************************************
+SUBROUTINE function_vortex(ni, nj, xcoor, ycoor, fnc_ana)
+!**************************************************************************************** 
+!!! VORTEX FROM TEMPEST-REMAP (as in XIOS)
   !
-END SUBROUTINE function_ana
+  INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND(12,307) ! double
+  !
+  INTEGER, INTENT(IN) :: ni, nj
+  REAL(kind=wp), DIMENSION(ni,nj), INTENT(IN)  :: xcoor, ycoor
+  REAL(kind=wp), DIMENSION(ni,nj), INTENT(OUT) :: fnc_ana
+
+  REAL(kind=wp), PARAMETER :: dp_pi = 3.14159265359
+  REAL(kind=wp), PARAMETER :: dLon0 = 5.5
+  REAL(kind=wp), PARAMETER :: dLat0 = 0.2
+  REAL(kind=wp), PARAMETER :: dR0   = 3.0
+  REAL(kind=wp), PARAMETER :: dD    = 5.0
+  REAL(kind=wp), PARAMETER :: dT    = 6.0
+  REAL(kind=wp) :: dp_length, dp_conv
+
+  REAL(kind=wp) :: dSinC, dCosC, dCosT, dSinT
+  REAL(kind=wp) :: dTrm, dX, dY, dZ
+  REAL(kind=wp) :: dlon, dlat
+  REAL(kind=wp) :: dRho, dVt, dOmega
+
+  INTEGER          :: i,j
+  CHARACTER(LEN=7) :: cl_anaftype="vortex"
+
+  dp_conv = dp_pi/180.
+  dSinC = SIN( dLat0 )
+  dCosC = COS( dLat0 )
+
+  DO j=1,nj
+     DO i=1,ni
+        ! Find the rotated longitude and latitude of a point on a sphere
+        !		with pole at (dLon0, dLat0).
+        dCosT = COS( ycoor(i,j)*dp_conv )
+        dSinT = SIN( ycoor(i,j)*dp_conv )
+        
+        dTrm = dCosT * COS( xcoor(i,j)*dp_conv - dLon0 )
+        dX   = dSinC * dTrm - dCosC * dSinT
+        dY   = dCosT * SIN( xcoor(i,j)*dp_conv - dLon0 )
+        dZ   = dSinC * dSinT + dCosC * dTrm
+        
+        dlon = ATAN2( dY, dX )
+        IF( dlon < 0.0 ) dlon = dlon + 2.0 * dp_pi
+        dlat = ASIN( dZ )
+        
+        dRho = dR0 * COS(dlat)
+        dVt = 3.0 * SQRT(3.0)/2.0/COSH(dRho)/COSH(dRho)*TANH(dRHo)
+        IF (dRho == 0.0) THEN
+           dOmega = 0.0
+        ELSE
+           dOmega = dVt / dRho
+        END IF
+        
+        fnc_ana(i,j) = 2.0 * ( 1.0 + TANH( dRho / dD * SIN( dLon - dOmega * dT ) ) )
+        
+     END DO
+  END DO
+  !
+END SUBROUTINE function_vortex
+
+END MODULE function_ana
--- examples/regrid_environment/read_all_data.F90
+++ examples/regrid_environment/read_all_data.F90
@@ -22,11 +22,13 @@ SUBROUTINE read_dimgrid (nlon,nlat,data_filename,cl_grd,w_unit,file_debug)
      imask_dims_ids,lon_dims_len,&
      lat_dims_len,imask_dims_len  
   !               
-  INTEGER, INTENT(out)     :: nlon,nlat
+  INTEGER, INTENT(out)       :: nlon,nlat
   !
-  CHARACTER(len=30)        :: data_filename
-  CHARACTER(len=4)         :: cl_grd ! name of the source grid
-  CHARACTER(len=8)         :: cl_nam ! cl_grd+.lon,+.lat ...
+  logical                    :: exists
+  CHARACTER(len=30)          :: data_filename
+  CHARACTER(len=4)           :: cl_grd ! name of the source grid
+  CHARACTER(len=8)           :: cl_nam ! cl_grd+.lon,+.lat ...
+  character(len=*),parameter :: subname = '(read_dimgrid)'
   !
   IF (file_debug) THEN
       WRITE(w_unit,*) 'Data_filename :',data_filename
@@ -35,37 +37,44 @@ SUBROUTINE read_dimgrid (nlon,nlat,data_filename,cl_grd,w_unit,file_debug)
   !
   ! Dimensions
   !
-  CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), __LINE__ )
+  ! Check if file exists before open it
+  inquire(file=trim(data_filename),exist=exists)
+  if (exists .eqv. .FALSE. ) then
+     write(w_unit,*) 'File ',trim(data_filename),' does not exists'
+     call routine_model_abort(w_unit,__FILE__,__LINE__,subname)
+  endif
+
+  CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), w_unit, subname, __FILE__,__LINE__ )
   !
   cl_nam=cl_grd//".lon" 
   IF (file_debug) THEN
       WRITE(w_unit,*) 'Longitudes :',cl_nam
       CALL FLUSH(w_unit)
   ENDIF
-  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam,  il_lon_id),    __LINE__ )
+  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam,  il_lon_id), w_unit, subname, __FILE__, __LINE__ )
   cl_nam=cl_grd//".lat" 
   IF (file_debug) THEN
       WRITE(w_unit,*) 'Latitudes :',cl_nam
       CALL FLUSH(w_unit)
   ENDIF
-  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam,  il_lat_id),    __LINE__ )
+  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam,  il_lat_id), w_unit, subname, __FILE__,   __LINE__ )
 
-  CALL hdlerr( NF90_INQUIRE_VARIABLE(il_file_id, varid=il_lon_id, ndims=lon_dims, dimids=lon_dims_ids), __LINE__ )
+  CALL hdlerr( NF90_INQUIRE_VARIABLE(il_file_id, varid=il_lon_id, ndims=lon_dims, dimids=lon_dims_ids), w_unit, subname, __FILE__, __LINE__ )
   !
   ! The value lon_dims_len(i) is obtained thanks to the lon_dims_ids ID already obtained from the file
   DO i=1,lon_dims
-    CALL hdlerr( NF90_INQUIRE_DIMENSION(ncid=il_file_id,dimid=lon_dims_ids(i),len=lon_dims_len(i)), __LINE__ )
+    CALL hdlerr( NF90_INQUIRE_DIMENSION(ncid=il_file_id,dimid=lon_dims_ids(i),len=lon_dims_len(i)), w_unit, subname, __FILE__, __LINE__ )
   ENDDO
   !
   nlon=lon_dims_len(1)
   nlat=lon_dims_len(2)
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   !
-  CALL hdlerr( NF90_INQUIRE_VARIABLE(ncid=il_file_id, varid=il_lat_id, ndims=lat_dims, dimids=lat_dims_ids), __LINE__ )
+  CALL hdlerr( NF90_INQUIRE_VARIABLE(ncid=il_file_id, varid=il_lat_id, ndims=lat_dims, dimids=lat_dims_ids), w_unit, subname, __FILE__, __LINE__ )
   !
   ! The value lat_dims_len(i) is obtained thanks to the lat_dims_ids ID already obtained from the file
   DO i=1,lat_dims
-    CALL hdlerr( NF90_INQUIRE_DIMENSION(ncid=il_file_id,dimid=lat_dims_ids(i),len=lat_dims_len(i)), __LINE__ )
+    CALL hdlerr( NF90_INQUIRE_DIMENSION(ncid=il_file_id,dimid=lat_dims_ids(i),len=lat_dims_len(i)), w_unit, subname, __FILE__, __LINE__ )
   ENDDO
   !
   IF ( (lat_dims_len(1) .NE. lon_dims_len(1)).OR.(lat_dims_len(2) .NE. lon_dims_len(2)) ) THEN
@@ -75,7 +84,7 @@ SUBROUTINE read_dimgrid (nlon,nlat,data_filename,cl_grd,w_unit,file_debug)
       STOP
   ENDIF
   !
-  CALL hdlerr(NF90_CLOSE(il_file_id), __LINE__ )
+  CALL hdlerr(NF90_CLOSE(il_file_id),  w_unit, subname, __FILE__, __LINE__ )
   !
   IF (file_debug) THEN
       WRITE(w_unit,*) 'Reading input file ',data_filename
@@ -94,16 +103,18 @@ END SUBROUTINE read_dimgrid
   !
   INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND(12,307) ! double
   !
-  INTEGER                  :: i,j,k,w_unit
-  LOGICAL                  :: file_debug
+  INTEGER                    :: i,j,k,w_unit
+  LOGICAL                    :: file_debug
   !
-  INTEGER                  :: il_file_id,il_lon_id, il_lat_id                                      
+  INTEGER                    :: il_file_id,il_lon_id, il_lat_id                                      
   !               
-  INTEGER, INTENT(in)     :: nlon,nlat
+  INTEGER, INTENT(in)        :: nlon,nlat
   !
-  CHARACTER(len=30)        :: data_filename
-  CHARACTER(len=4)         :: cl_grd ! name of the source grid
-  CHARACTER(len=8)         :: cl_nam ! cl_grd+.lon,+.lat ...
+  logical                    :: exists
+  CHARACTER(len=30)          :: data_filename
+  CHARACTER(len=4)           :: cl_grd ! name of the source grid
+  CHARACTER(len=8)           :: cl_nam ! cl_grd+.lon,+.lat ...
+  character(len=*),parameter :: subname = '(read_grid)'
   !
   INTEGER,  DIMENSION(2)   :: ila_dim
   INTEGER,  DIMENSION(2)   :: ila_what
@@ -116,22 +127,28 @@ END SUBROUTINE read_dimgrid
       CALL FLUSH(w_unit)
   ENDIF
   !
-  ! Dimensions
+  ! Check if file exists before open it
+  inquire(file=trim(data_filename),exist=exists)
+  if (exists .eqv. .FALSE. ) then
+     write(w_unit,*) 'File ',trim(data_filename),' does not exists'
+     call routine_model_abort(w_unit,__FILE__,__LINE__,subname)
+  endif
+
+  CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), w_unit, subname, __FILE__,__LINE__ )
   !
-  CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), __LINE__ )
   !
   cl_nam=cl_grd//".lon" 
   IF (file_debug) THEN
       WRITE(w_unit,*) 'Longitudes :',cl_nam
       CALL FLUSH(w_unit)
   ENDIF
-  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam,  il_lon_id),    __LINE__ )
+  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam,  il_lon_id),  w_unit, subname, __FILE__, __LINE__ )
   cl_nam=cl_grd//".lat" 
   IF (file_debug) THEN
       WRITE(w_unit,*) 'Latitudes :',cl_nam
       CALL FLUSH(w_unit)
   ENDIF
-  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam,  il_lat_id),    __LINE__ )
+  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam,  il_lat_id),  w_unit, subname, __FILE__, __LINE__ )
   !
   ila_what(:)=1
   !
@@ -140,23 +157,22 @@ END SUBROUTINE read_dimgrid
   !
   ! Data
   !
-  !
   CALL hdlerr( NF90_GET_VAR (il_file_id, il_lon_id, gridlon, &
-     ila_what, ila_dim), __LINE__ )
+     ila_what, ila_dim),  w_unit, subname, __FILE__, __LINE__ )
   IF (file_debug) THEN
       WRITE(w_unit,*) '... global grid longitudes reading done'
       CALL FLUSH(w_unit)
   ENDIF
   !
   CALL hdlerr( NF90_GET_VAR (il_file_id, il_lat_id, gridlat, &
-     ila_what, ila_dim), __LINE__ )
+     ila_what, ila_dim),  w_unit, subname, __FILE__, __LINE__ )
   IF (file_debug) THEN
       WRITE(w_unit,*) '... global grid latitudes reading done'
       CALL FLUSH(w_unit)
   ENDIF
   !
   !
-  CALL hdlerr( NF90_CLOSE(il_file_id), __LINE__ )
+  CALL hdlerr( NF90_CLOSE(il_file_id),  w_unit, subname, __FILE__, __LINE__ )
   !
   IF (file_debug) THEN
       WRITE(w_unit,*) 'End of routine read_grid'
@@ -165,35 +181,79 @@ END SUBROUTINE read_dimgrid
   !
 END SUBROUTINE read_grid
 
+!****************************************************************************************
+LOGICAL FUNCTION inquire_frac (data_filename, cl_grd, w_unit, file_debug)
+!**************************************************************************************
+   !
+   INTEGER                    :: w_unit
+   LOGICAL                    :: file_debug
+   !
+   INTEGER                    :: il_file_id, il_indice_id
+   !
+   logical                    :: exists               
+   CHARACTER(len=30)          :: data_filename
+   CHARACTER(len=*)           :: cl_grd ! name of the source grid
+   CHARACTER(len=8)           :: cl_nam ! cl_grd+.frc
+   character(len=*),parameter :: subname = '(inquire_frac)'
+   !
+   ! Check if file exists before open it
+   inquire(file=trim(data_filename),exist=exists)
+   if (exists .eqv. .FALSE. ) then
+      write(w_unit,*) 'File ',trim(data_filename),' does not exists'
+      call routine_model_abort(w_unit,__FILE__,__LINE__,subname)
+   endif
+
+   CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), w_unit, subname, __FILE__,__LINE__ )
+   !
+   !
+   cl_nam=TRIM(cl_grd)//".frc" 
+   inquire_frac =  NF90_INQ_VARID(il_file_id, TRIM(cl_nam),  il_indice_id)  == NF90_NOERR
+   !
+   CALL hdlerr( NF90_CLOSE(il_file_id),  w_unit, subname, __FILE__, __LINE__ )
+   !
+   IF (file_debug) THEN
+      WRITE(w_unit,*) 'End of function inquire_frac for ',TRIM(cl_grd)
+      CALL FLUSH(w_unit)
+   ENDIF
+   !
+END FUNCTION inquire_frac
+
   !****************************************************************************************
   SUBROUTINE read_mask (nlon, nlat, &
                         data_filename, cl_grd, w_unit, file_debug, &
                         indice_mask)
   !**************************************************************************************
   !
-  INTEGER                  :: i,j,k,w_unit
-  LOGICAL                  :: file_debug
+  INTEGER                    :: i,j,k,w_unit
+  LOGICAL                    :: file_debug
   !
-  INTEGER                  :: il_file_id, il_indice_id
+  INTEGER                    :: il_file_id, il_indice_id
   !               
-  INTEGER, INTENT(in)     :: nlon,nlat
+  INTEGER, INTENT(in)        :: nlon,nlat
   !
-  CHARACTER(len=30)        :: data_filename
-  CHARACTER(len=4)         :: cl_grd ! name of the source grid
-  CHARACTER(len=8)         :: cl_nam ! cl_grd+.lon,+.lat ...
+  logical                    :: exists
+  CHARACTER(len=30)          :: data_filename
+  CHARACTER(len=*)           :: cl_grd ! name of the source grid
+  CHARACTER(len=8)          :: cl_nam ! cl_grd+.lon,+.lat ...
+  character(len=*),parameter :: subname = '(read_mask)'
   !
   INTEGER,  DIMENSION(2)   :: ila_dim
   INTEGER,  DIMENSION(2)   :: ila_what
   !
   INTEGER, DIMENSION(nlon,nlat)  :: indice_mask
   !
+  ! Check if file exists before open it
+  inquire(file=trim(data_filename),exist=exists)
+  if (exists .eqv. .FALSE. ) then
+     write(w_unit,*) 'File ',trim(data_filename),' does not exists'
+     call routine_model_abort(w_unit,__FILE__,__LINE__,subname)
+  endif
+
+  CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), w_unit, subname, __FILE__,__LINE__ )
   !
-  ! Dimensions
   !
-  CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), __LINE__ )
-  !
-  cl_nam=cl_grd//".msk" 
-  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam,  il_indice_id),    __LINE__ )
+  cl_nam=TRIM(cl_grd)//".msk" 
+  CALL hdlerr( NF90_INQ_VARID(il_file_id, TRIM(cl_nam),  il_indice_id),  w_unit, subname, __FILE__, __LINE__ )
   !
   ila_what(:)=1
   !
@@ -203,13 +263,13 @@ END SUBROUTINE read_grid
   ! Data
   !
   CALL hdlerr( NF90_GET_VAR (il_file_id, il_indice_id, indice_mask, &
-     ila_what, ila_dim), __LINE__ )
+     ila_what, ila_dim),  w_unit, subname, __FILE__, __LINE__ )
   IF (file_debug) THEN
       WRITE(w_unit,*) '... global grid mask reading done'
       CALL FLUSH(w_unit)
   ENDIF
   !
-  CALL hdlerr( NF90_CLOSE(il_file_id), __LINE__ )
+  CALL hdlerr( NF90_CLOSE(il_file_id),  w_unit, subname, __FILE__, __LINE__ )
   !
   IF (file_debug) THEN
       WRITE(w_unit,*) 'End of routine read_mask'
@@ -226,16 +286,18 @@ END SUBROUTINE read_mask
   !
   INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND(12,307) ! double
   !
-  INTEGER                  :: i,j,k,w_unit
-  LOGICAL                  :: file_debug
+  INTEGER                    :: i,j,k,w_unit
+  LOGICAL                    :: file_debug
   !
-  INTEGER                  :: il_file_id,  il_srf_id
+  INTEGER                    :: il_file_id,  il_srf_id
   !               
-  INTEGER, INTENT(in)     :: nlon,nlat
+  INTEGER, INTENT(in)        :: nlon,nlat
   !
-  CHARACTER(len=30)        :: data_filename
-  CHARACTER(len=4)         :: cl_grd ! name of the source grid
-  CHARACTER(len=8)         :: cl_nam ! cl_grd+.lon,+.lat ...
+  logical                    :: exists
+  CHARACTER(len=30)          :: data_filename
+  CHARACTER(len=4)           :: cl_grd ! name of the source grid
+  CHARACTER(len=8)           :: cl_nam ! cl_grd+.lon,+.lat ...
+  character(len=*),parameter :: subname = '(read_area)'
   !
   INTEGER,  DIMENSION(2)   :: ila_dim
   INTEGER,  DIMENSION(2)   :: ila_what
@@ -243,12 +305,17 @@ END SUBROUTINE read_mask
   REAL (kind=wp), DIMENSION(nlon,nlat)  :: gridsrf
   !
   !
-  ! Dimensions
-  !
-  CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), __LINE__ )
+  ! Check if file exists before open it
+  inquire(file=trim(data_filename),exist=exists)
+  if (exists .eqv. .FALSE. ) then
+     write(w_unit,*) 'File ',trim(data_filename),' does not exists'
+     call routine_model_abort(w_unit,__FILE__,__LINE__,subname)
+  endif
+
+  CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), w_unit, subname, __FILE__,__LINE__ )
   !
   cl_nam=cl_grd//".srf" 
-  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam, il_srf_id),    __LINE__ )
+  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam, il_srf_id),  w_unit, subname, __FILE__, __LINE__ )
   !
   ila_what(:)=1
   !
@@ -258,13 +325,13 @@ END SUBROUTINE read_mask
   ! Data
   !
   CALL hdlerr( NF90_GET_VAR (il_file_id, il_srf_id, gridsrf, &
-     ila_what, ila_dim), __LINE__ )
+     ila_what, ila_dim),  w_unit, subname, __FILE__, __LINE__ )
   IF (file_debug) THEN
       WRITE(w_unit,*) '... global grid mask reading done'
       CALL FLUSH(w_unit)
   ENDIF
   !
-  CALL hdlerr( NF90_CLOSE(il_file_id), __LINE__ )
+  CALL hdlerr( NF90_CLOSE(il_file_id),  w_unit, subname, __FILE__, __LINE__ )
   !
   IF (file_debug) THEN
       WRITE(w_unit,*) 'End of routine read_mask'
@@ -272,6 +339,61 @@ END SUBROUTINE read_mask
   ENDIF
   !
 END SUBROUTINE read_area
+
+  !****************************************************************************************
+  SUBROUTINE read_frac (nlon,nlat, &
+                        data_filename, cl_grd, w_unit, file_debug, &
+                        gridfrc)
+  !****************************************************************************************
+  !
+  INTEGER, PARAMETER :: wp = SELECTED_REAL_KIND(12,307) ! double
+  !
+  INTEGER                  :: i,j,k,w_unit
+  LOGICAL                  :: file_debug
+  !
+  INTEGER                  :: il_file_id,  il_srf_id
+  !               
+  INTEGER, INTENT(in)     :: nlon,nlat
+  !
+  CHARACTER(len=30)        :: data_filename
+  CHARACTER(len=*)         :: cl_grd ! name of the source grid
+  CHARACTER(len=8)         :: cl_nam ! cl_grd+.lon,+.lat ...
+  !
+  INTEGER,  DIMENSION(2)   :: ila_dim
+  INTEGER,  DIMENSION(2)   :: ila_what
+  !
+  REAL (kind=wp), DIMENSION(nlon,nlat)  :: gridfrc
+  !
+  !
+  ! Dimensions
+  !
+  CALL hdlerr(NF90_OPEN(data_filename, NF90_NOWRITE, il_file_id), __LINE__ )
+  !
+  cl_nam=cl_grd//".frc"
+  CALL hdlerr( NF90_INQ_VARID(il_file_id, cl_nam, il_srf_id),    __LINE__ )
+  !
+  ila_what(:)=1
+  !
+  ila_dim(1)=nlon
+  ila_dim(2)=nlat
+  !
+  ! Data
+  !
+  CALL hdlerr( NF90_GET_VAR (il_file_id, il_srf_id, gridfrc, &
+     ila_what, ila_dim), __LINE__ )
+  IF (file_debug) THEN
+      WRITE(w_unit,*) '... global grid frac reading done'
+      CALL FLUSH(w_unit)
+  ENDIF
+  !
+  CALL hdlerr( NF90_CLOSE(il_file_id), __LINE__ )
+  !
+  IF (file_debug) THEN
+      WRITE(w_unit,*) 'End of routine read_frac'
+      CALL FLUSH(w_unit)
+  ENDIF
+  !
+END SUBROUTINE read_frac
 
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 END MODULE read_all_data
