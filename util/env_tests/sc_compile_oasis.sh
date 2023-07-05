528b8940a1acac3a00c55212ad32441666a6b0b6
coquart <coquart@cerfacs.fr>
2023-07-05 15:18:04 +0200

    Update to compile either oasis3-mct (default) either pyoasis Signed-off-by: coquart <coquart@cerfacs.fr>
    Former-commit-id: 365f05a3ff0e5b9d1bcde500ed8b3b8feb100e58

--- util/env_tests/sc_compile_oasis.sh
+++ util/env_tests/sc_compile_oasis.sh
@@ -5,12 +5,10 @@ cd ${OASIS_COUPLE}/util/make_dir
 make realclean -f ${OASIS_COUPLE}/util/make_dir/TopMakefileOasis3
 make -f ${OASIS_COUPLE}/util/make_dir/TopMakefileOasis3 $OASIS_TARGET
 
-# ATTENTION : si oasis3-mct librairies statiques
-# sinon verification creation de 
-# libmct.so  libmpeu.so  liboasis.cbind.so  libpsmile.MPI1.so  libscrip.so
+if [ -z "${OASIS_TARGET}" ]; then
+# results in INSTALL_OASIS.${OASIS_ENV}: build-static  include  lib
+#
 ls ${OASIS_COUPLE}/INSTALL_OASIS.${OASIS_ENV}/lib/libmct.a
-# A tester
-#res_command=`echo $?`
 if [ `echo $?` -ne 0 ]; then
     echo "pb libmct.a not created"
     exit 1
@@ -32,4 +30,39 @@ res_command=`echo $?`
 if [ ${res_command} -ne 0 ]; then
     echo "libscrip.a not created"
     exit 1
+fi
+else
+# results in INSTALL_OASIS.${OASIS_ENV} : build-shared  include  lib  python
+#
+ls ${OASIS_COUPLE}/INSTALL_OASIS.${OASIS_ENV}/lib/libmct.so
+if [ `echo $?` -ne 0 ]; then
+    echo "pb libmct.a not created"
+    exit 1
+fi
+ls ${OASIS_COUPLE}/INSTALL_OASIS.${OASIS_ENV}/lib/libmpeu.so
+res_command=`echo $?`
+if [ ${res_command} -ne 0 ]; then
+    echo "pb libmpeu.a  not created"
+    exit 1
+fi
+ls ${OASIS_COUPLE}/INSTALL_OASIS.${OASIS_ENV}/lib/libscrip.so
+res_command=`echo $?`
+if [ ${res_command} -ne 0 ]; then
+    echo "libpsmile.MPI1.a not created"
+    exit 1
+fi
+ls ${OASIS_COUPLE}/INSTALL_OASIS.${OASIS_ENV}/lib/libpsmile.MPI1.so
+res_command=`echo $?`
+if [ ${res_command} -ne 0 ]; then
+    echo "libscrip.a not created"
+    exit 1
+fi
+ls ${OASIS_COUPLE}/INSTALL_OASIS.${OASIS_ENV}/lib/liboasis.cbind.so
+res_command=`echo $?`
+if [ ${res_command} -ne 0 ]; then
+    echo "libscrip.a not created"
+    exit 1
+fi
+#
+# End else test on pyoasis
 fi
