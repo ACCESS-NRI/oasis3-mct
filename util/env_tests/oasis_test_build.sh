ed9e7b4af185a32c4714261230b4a827fcb9c066
valcke <valcke@cerfacs.fr>
2023-08-09 13:34:21 +0200

    Added use of param file and copy of Makefile (/)
    Former-commit-id: 990a33a8e2ed50c1938fa89b25291440926375bb

--- util/env_tests/oasis_test_build.sh
+++ util/env_tests/oasis_test_build.sh
@@ -2,17 +2,31 @@
 ###############################################
 # USAGE : 
 # Define following the variable environment:
+# OASIS_COUPLE : the location of the sources of the coupler (/lib, /util, /examples, etc. directories) 
+# OASIS_ENV :the extension of the header Makefile to use for OASIS3-MCT compilation
 #
-# For all the toys
-# export OASIS_COUPLE=
-# export OASIS_ENV=
+# Then for example
+# ./oasis_test_build.sh param_casename_test01
+# ./oasis_test_build.sh param_casename_test02
+# ...
 #
-# Then
-# ./oasis_test_build.sh
+# The param_casename_test?? are files that exists in the toy
+# directory and specify several aspects of the each test run
 #
-# Be carefull that OASIS3-MCT is compiled
-# with the same environment than the toy
+# Be carefull that OASIS3-MCT is compiled with the same environment than the toy
 ###############################################
+
+testname=$1
+echo "testname = $testname"
+if [ ! -f ./$testname ]; then
+  echo "ERROR in param file argument, usage"
+  echo "  ./oasis_test_build.sh \$testname"
+  exit -9
+fi
+
+srcdir=`pwd`
+export casename=`basename $srcdir`
+export pathname=`dirname $srcdir`
 
 if [ -z "${OASIS_ENV}" ]; then
   echo "ERROR OASIS_ENV not defined"
@@ -24,12 +38,17 @@ if [ -z "${OASIS_COUPLE}" ]; then
   exit -9
 fi
 
+if [ -z "${OASIS_TOYDIR}" ]; then
+  export OASIS_TOYDIR=`pwd`
+  echo "OASIS_TOYDIR is by default ${OASIS_TOYDIR}"
+else
+  echo "OASIS_TOYDIR is set in sc_top.sh and is ${OASIS_TOYDIR}"
+fi
 
 . ${OASIS_COUPLE}/util/make_dir/comp_env_${OASIS_ENV}.sh
 
-#echo "module setting"
-#module list
-
+. ./$testname
+cp -f ${USER_MAKELOC}/${USER_MAKEFILE} ${OASIS_TOYDIR}/Makefile
 make clean
 make
 
