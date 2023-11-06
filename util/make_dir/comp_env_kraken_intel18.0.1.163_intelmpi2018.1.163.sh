dca73cd0f31ac4de280d710fbf06a36166b1a404
Andrea Piacentini <piacentini.palm@gmail.com>
2023-11-06 12:07:03 +0100

    [yac_remap] Allows for interactive tests on kglobc1
    Former-commit-id: cf683d8f58de7683b00fb5e662e343ed2138bd19

--- util/make_dir/comp_env_kraken_intel18.0.1.163_intelmpi2018.1.163.sh
+++ util/make_dir/comp_env_kraken_intel18.0.1.163_intelmpi2018.1.163.sh
@@ -5,7 +5,11 @@
 source /etc/profile.d/modules.sh
 module purge
 module load compiler/intel/18.0.1.163
-module load mpi/intelmpi/2018.1.163
+if [ ${HOSTNAME} == kglobc1 ] ; then
+  module load mpi/intelmpi/2018.1.163_ssh
+else
+  module load mpi/intelmpi/2018.1.163
+fi
 module load lib/netcdf-fortran/4.4.4_impi
 module load lib/phdf5/1.8.20_impi 
 module load python/3.7.7
