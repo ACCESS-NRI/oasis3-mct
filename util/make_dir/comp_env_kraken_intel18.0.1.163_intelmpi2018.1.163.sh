daf46bf397242053266388ebe5805dd9111275fe
Gabriel Jonville <gabriel.jonville@cerfacs.fr>
2024-03-19 11:37:56 +0100

    Specify intel18 compilation environment on kraken machine
    Former-commit-id: bceb4b45f147a0acf72d18740314f0622b961c0d

--- util/make_dir/comp_env_kraken_intel18.0.1.163_intelmpi2018.1.163.sh
+++ util/make_dir/comp_env_kraken_intel18.0.1.163_intelmpi2018.1.163.sh
@@ -5,11 +5,19 @@
 source /etc/profile.d/modules.sh
 module purge
 module load compiler/intel/18.0.1.163
-module load mpi/intelmpi/2018.1.163
+
+if [ ${HOSTNAME} == kglobc1 ] ; then
+  module load mpi/intelmpi/2018.1.163_ssh
+  echo 'We work on kglobc1 node'
+  export corespn=64
+else
+  module load mpi/intelmpi/2018.1.163
+  echo 'We work on kraken machine'
+  export corespn=36
+fi
+
+echo `which mpirun`
+export MPIRUN=mpirun
 module load lib/netcdf-fortran/4.4.4_impi
 module load lib/phdf5/1.8.20_impi 
 module load python/3.7.7
-echo 'We work on Kraken'
-echo `which mpirun`
-export MPIRUN=mpirun
-export corespn=36
