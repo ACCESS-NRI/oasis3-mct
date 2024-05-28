125bf6efd3a320f0f2a1595f7eb6d2673f261627
Gabriel Jonville <gabriel.jonville@cerfacs.fr>
2024-05-28 16:42:59 +0200

    [yac_remap] adjust the compilation environment on scylla cluster
    Former-commit-id: 1d0e32838dcc00b4004f6bd792f59716c588fc58

--- util/make_dir/comp_env_scylla_intel18.0.5.274_intelmpi2018.4.274.sh
+++ util/make_dir/comp_env_scylla_intel18.0.5.274_intelmpi2018.4.274.sh
@@ -4,11 +4,13 @@
 ##########################
 source /etc/profile.d/modules.sh
 module purge
+module load compiler/gcc/5.4.0
 module load compiler/intel/18.0.5.274
-module load mpi/intelmpi/2018.4.274 
-module load lib/netcdf-fortran/4.4.4 lib/hdf5/1.8.20_intel
+module load mpi/intelmpi/2018.4.274
+module load lib/phdf5/1.8.20_impi
+module load lib/netcdf-fortran/4.4.4
 module load python/3.7.7
-echo 'We work on scylla AMD node'
+echo 'We work on scylla cluster'
 echo `which mpirun`
 export MPIRUN=mpirun
-export corespn=16
+export corespn=28
