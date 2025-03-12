6a3efc4a66dd523ccc97007b1409edf8475f39ed
Gabriel Jonville <gabriel.jonville@cerfacs.fr>
2025-03-12 15:49:12 +0100

    Add gnu11.2 + openmpi4.1.1 configuration for kraken machine and associated test environment files
    Former-commit-id: a2968db28232549c18fc27db5f65a599f324568b

---
+++ util/make_dir/comp_env_kraken_gcc11.2_openmpi4.1.1.sh
@@ -0,0 +1,19 @@
+#!/bin/ksh
+##########################
+# Compilation environment
+##########################
+source /etc/profile.d/modules.sh
+module purge
+module load compiler/gcc/11.2.0
+module load mpi/openmpi/4.1.1_gcc112
+if [ `hostname` == kglobc1 ] ; then
+  echo 'We work on kglobc1 node'
+  export corespn=64
+else
+  echo 'We work on kraken machine'
+  export corespn=36
+fi
+echo `which mpirun`
+export MPIRUN=mpirun
+module load lib/phdf5/1.10.8_gcc112_ompi411
+module load lib/netcdf-fortran/4.4.4_phdf5_1.10.8_ompi_gcc112
