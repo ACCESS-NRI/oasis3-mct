622704751e3d275a52b6705d1187231eaae4d41e
Gabriel Jonville <gabriel.jonville@cerfacs.fr>
2025-03-17 14:14:42 +0100

    Add environment files for the CI on calypso machine (intel23+intelmpi21)
    Former-commit-id: f93ed00b6935eb9549daa474c7c9bbb2e62791b3

--- util/make_dir/comp_env_calypso_intel23.2.0_intelmpi2021.10.0.sh
+++ util/make_dir/comp_env_calypso_intel23.2.0_intelmpi2021.10.0.sh
@@ -3,22 +3,22 @@
 # Compilation environment
 ##########################
 source /etc/profile.d/modules.sh
-module purge
-module load tbb/latest
-module load compiler-rt/latest
-module load oclfpga/latest
-module load compiler/2023.2.0
-module load mkl/2023.2.0
-module load mpi/2021.10.0
+module purge > /dev/null
+module load tbb/latest > /dev/null
+module load compiler-rt/latest > /dev/null
+module load oclfpga/latest > /dev/null
+module load compiler/2023.2.0 > /dev/null
+module load mkl/2023.2.0 > /dev/null
+module load mpi/2021.10.0 > /dev/null
 if [ `hostname` == calypso-globc1.calypso.cerfacs.fr ] ; then
   echo 'We work on calypso-globc1 node'
   export corespn=64
 else
-  echo 'We work on kraken machine'
+  echo 'We work on calypso machine'
   export corespn=192
 fi
 echo `which mpirun`
 export MPIRUN=mpirun
 export I_MPI_COLL_EXTERNAL=no
-module load lib/phdf5/1.12.0_impi
-module load lib/netcdf-fortran/4.4.4_phdf5_1.12.0
+module load lib/phdf5/1.12.0_impi > /dev/null
+module load lib/netcdf-fortran/4.4.4_phdf5_1.12.0 > /dev/null
